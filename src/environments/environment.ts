3d7713bc9416889363b076983447e5fdbb6ea873
yokam03 <nieles_official@yahoo.com>
2018-03-07 15:39:21 +0800

    modifies the app routing and user profile auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+    apiKey: "AIzaSyCVISdyDgRfBc4u7Kri6SLo8rNdhkb7-kU",
+    authDomain: "finalweb-f6d09.firebaseapp.com",
+    databaseURL: "https://finalweb-f6d09.firebaseio.com",
+    projectId: "finalweb-f6d09",
+    storageBucket: "finalweb-f6d09.appspot.com",
+    messagingSenderId: "679007966037"
     }
 };
